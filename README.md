ab684fef19cdca4f6dbc9ee37aa4887f66312dac
Theodor Simonsen <theodor@snedker.info>
2018-11-22 08:45:23 +0100

    add kodebase

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # kodebase-TheodorSimonsen
-basic kodebase for TheodorSkimonsen
+
+## Projektbeskrivelse
+
+En kodebase til fremtidige projekter, for hjemmesider. 
+projektet er til server baseret sider, med smooth URL strukture.
+
+## NPM Pakker
+
+* express
+* nodemon
+* fs
+* bcrypt
+* pjson
+* debug
+* express-session
+* mysql
+* morgan
+* body-parser
+
+## Kommandoer
+
+* Start projektet `npm init` 
+* Download pakker `npm i / npm install`
+* Start server: `npm run dev`
+
+## Relevante kode-eksempler
+
+```javascript
+app.get('/URL', (req, res, next) => {
+    res.render('EjsNavn');
+});
+```
+
+## Maintainers
+
+* Theodor Simonsen
---
+++ server.js
@@ -0,0 +1,53 @@
+// IMPORTS (NPM I)
+//===========================================================
+const express = require('express');
+const bcrypt = require('bcrypt');
+const fs = require('fs');
+const pjson = require('./package.json');
+const debug = require('debug');
+const session = require('express-session');
+const mysql = require('mysql');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const port = process.env.PORT || 1239;
+
+// SERVER
+//===========================================================
+const app = express();
+
+// CONFIQ SHIT
+//===========================================================
+
+app.set('views', 'views');           // In which folder are views located
+app.set('view engine', 'ejs');       // Which view engine to use
+app.use(express.static('./public')); // Where are static files located
+
+app.use(bodyParser.json());          // Accept JSON objects
+
+// Accept extended form elements in requests
+app.use(bodyParser.urlencoded({
+	'extended': true
+}));
+
+// Setup session handling
+app.use(session({
+	'resave': false,
+	'saveUninitialized': true,
+	'secret': 'really secret stuffs'
+}));
+
+app.use(logger('dev'));	
+
+
+// ROUTES
+//===========================================================
+
+app.get('/', (req, res, next) => {
+    res.render('page');
+});
+
+// SERVER INIT
+// ==========================================================
+app.listen(port, () => {
+    console.log('Yep, its working')
+});
